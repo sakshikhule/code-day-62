6256ef47bf50e4eeb314264312309bb2e82024aa
sakshikhule <102469697+sakshikhule@users.noreply.github.com>
2023-01-31 19:15:26 +0530

    Add files via upload

---
+++ day 62.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int test;
+ cin>>test;
+ while(test--){
+ int n,g,c;
+ cin>>n>>g>>c;
+ if(((n+1)*c)>=g){
+ cout<<"YES"<<endl;
+ }
+ else{
+ cout<<"NO"<<endl;
+ }
+}
+}
+
